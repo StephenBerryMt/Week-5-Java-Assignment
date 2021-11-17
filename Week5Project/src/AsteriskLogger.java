f1c54a604740cfef24152b8dc156d8278c00510e
Stephen Berry <92691419+StephenBerryMt@users.noreply.github.com>
2021-11-16 20:26:39 -0700

    Add files via upload

---
+++ Week5Project/src/AsteriskLogger.java
@@ -0,0 +1,35 @@
+
+//3.a	Create a AstriskLogger class that implement the Logger interface
+
+public class AsteriskLogger implements Logger {
+	
+//	4.	The log method on the AsteriskLogger should print out the String it receives between 3
+//	asterisks on either side of the String (e.g. if the String passed in is “Hello”, then it 
+//	should print ***Hello*** to the console.
+
+	@Override
+	public void Log(String Log) {
+		System.out.println("***" + Log + "***");
+		
+	}
+	
+//	5.	The error method on the AsteriskLogger should print the String it receives inside a box of
+//	asterisks, with the String preceded by the word “ERROR:”. For example, if “Hello” is the
+//	argument, the following should be printed:
+//	****************
+//	***Error: Hello***
+//	****************SSSSS
+
+
+	@Override
+	public void Error(String Error) {
+		System.err.println("\n******************");
+		System.err.println("");
+		System.err.println("***ERROR: " + Error + "***");
+		System.err.println("");
+		System.err.println("******************");
+		System.err.println("");
+		
+	}
+
+}
